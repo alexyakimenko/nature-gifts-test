9119ad41c25cc364324b38e83ce1cd81d640bd28
Alex <alexyakimenkojr@gmail.com>
2024-09-02 22:52:02 +0300

    add default theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "reset-css"
+import "./main.css"
+
+import "./themes/nature-gifts-default.css"
 
 createRoot(document.getElementById('root')).render(<App />)
